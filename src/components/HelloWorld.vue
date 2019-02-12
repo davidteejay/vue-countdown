045fac52a65a6558534cc4ae0f628f7a6694c89d
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2019-02-12 13:04:21 +0100

    I'm a motherfuckin genius

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,84 @@
 <template>
+<div>
   <div class="countdown">
     <h1 v-if="!timeUp">{{ minute }}:{{ second < 10 ? '0' + second : second }}</h1>
     <h1 v-else>Time's up</h1>
   </div>
+  <div class="quiz" :class="{show: isCurrent(i)}" v-for="(question, i) in questions" :key="i">
+    <h3>{{ question.question }}</h3>
+    <p>
+      <input :checked="isChecked('a')" @change="answer('a')" id="optionA" type="radio" :name="name(i + 1)">
+      <label for="optionA">{{ question.optionA }}</label>
+    </p>
+    <p>
+      <input :checked="isChecked('b')" @change="answer('b')" id="optionB" type="radio" :name="name(i + 1)">
+      <label for="optionB">{{ question.optionB }}</label>
+    </p>
+    <p>
+      <input :checked="isChecked('c')" @change="answer('c')" id="optionC" type="radio" :name="name(i + 1)">
+      <label for="optionC">{{ question.optionC }}</label>
+    </p>
+    <p>
+      <input :checked="isChecked('d')" @change="answer('d')" id="optionD" type="radio" :name="name(i + 1)">
+      <label for="optionD">{{ question.optionD }}</label>
+    </p>
+  </div>
+  <button @click="go(-1)">PREVIOUS</button>
+  <button @click="go(1)">NEXT</button>
+</div>
 </template>
 
 <script>
+import questions from './test'
+
 export default {
   name: 'HelloWorld',
   data(){
     return {
       minute: 20,
       second: 0,
-      timeUp: false
+      timeUp: false,
+      questions: [],
+      currentQuestion: 1,
+      answers: []
+
+    }
+  },
+  methods: {
+    isCurrent: function(index){
+      return this.timeUp === false && this.currentQuestion === index + 1
+    },
+    go: function(dir){
+      let { currentQuestion, questions } = this
+      if (dir === -1 && currentQuestion === 1) alert("This is the first question")
+      else if (dir === 1 && currentQuestion === questions.length) alert("This is the last question")
+      else this.currentQuestion = currentQuestion + dir
+    },
+    answer: function(answer){
+      let { answers, currentQuestion } = this
+
+      let search = answers.findIndex(answer => {
+        return answer.question === currentQuestion
+      })
+
+      answers[search] = { question: currentQuestion, answer }
+
+      this.answers = answers
+    },
+    name: function(option){
+      return `${option}option`
+    },
+    isChecked: function(option){
+      return this.answers[this.currentQuestion - 1].answer === option
     }
   },
   mounted(){
+    this.questions = questions
+
+    for (let i = 0; i < questions.length; i++){
+      this.answers.push({ question: i + 1, answer: '' })
+    }
+
     let timer = setInterval(() => {
       let { minute, second } = this
 
@@ -45,5 +108,13 @@ export default {
 
 h1 {
   width: 100%
+}
+
+.quiz {
+  display: none
+}
+
+.quiz.show {
+  display: block;
 }
 </style>
